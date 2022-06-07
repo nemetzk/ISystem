b51f010f40a1c24280701859a4582d3efcaf2554
Krisztian Nemet <nemetzk@gmail.com>
2022-06-07 07:00:27 +0200

    220607 reggel

--- Inc/mySerialProtocol.h
+++ Inc/mySerialProtocol.h
@@ -8,26 +8,67 @@
 #ifndef INC_MYSERIALPROTOCOL_H_
 #define INC_MYSERIALPROTOCOL_H_
 
+#include "timerFunctions.h"
+
+#define NO_STARTCHAR 	0
+#define NO_STATION	 	1
+#define NO_COMMAND 		2
+#define NO_ADDRESS 		3
+#define NO_VALUE		4
+#define NO_ENDCHAR		5
+
+#define ST_INIT 			0
+#define ST_RESET			1
+#define ST_WF_STARTCHAR 	2
+#define ST_WF_STATION	 	3
+#define ST_WF_COMMAND 		4
+#define ST_WF_ADDRESS 		5
+#define ST_WF_VALUE			6
+#define ST_WF_ENDCHAR		7
+
+
 typedef struct mspFuncth
 {
-	uint8_t SBUS_rxBuffer[25];
+	uint8_t protocol_st;
+	uint8_t RxBuffer[25];
 	uint8_t oneCharBuffer;
 	uint16_t elapsedTime;
 	uint8_t no_char;
+	struct UART_HandleTypeDef *Uart;
 	myTimerType sbusTimer;
 }mspFunct;
+
+typedef struct currentInFrameth
+{
+	uint8_t Station;
+	uint8_t Command;
+	uint8_t Address;
+	uint8_t Value;
+}currentInFramet;
+
+typedef struct protocolth
+{
+	uint8_t startChar;
+	uint8_t endChar;
+	uint8_t readChar;
+	uint8_t writeChar;
+
+
+}protocolt;
+
 
 typedef struct mySerialProtocolth
 {
 	uint8_t number;
-	struct UART_HandleTypeDef *Uart;
 	struct mspFuncth func;
+	struct protocolth protocol;
+	struct currentInFrameth currentInFrame;
 }mySerialProtocolt;
 
 
 #define MAX_NO_MYSERIALPROTOCOLS 2
 uint8_t no_mySerialProtocols;
-sbust *mySerialProtocols[MAX_NO_MYSERIALPROTOCOLS+1];
+mySerialProtocolt *mySerialProtocols[MAX_NO_MYSERIALPROTOCOLS+1];
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 
--- Src/mySerialProtocol.c
+++ Src/mySerialProtocol.c
@@ -12,7 +12,16 @@ void initMySerialProtocol(struct mySerialProtocolth *msp)
 	uint8_t i;
 	msp->number=++no_mySerialProtocols;
 	mySerialProtocols[no_mySerialProtocols]=msp;
-	HAL_UART_Receive_IT(msp->Uart, &(msp->func.oneCharBuffer), 1);
+	// ************ protocol *************
+
+	 msp->protocol.startChar = '&';
+	 msp->protocol.endChar = '!';
+	 msp->protocol.readChar = 'r';
+	 msp->protocol.writeChar = 'w';
+
+	 //******
+
+	HAL_UART_Receive_IT(msp->func.Uart, &(msp->func.oneCharBuffer), 1);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
@@ -20,21 +29,74 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 	uint8_t eidx;
 	struct mySerialProtocolth *msp;
 
-	for (eidx=1;eidx<=no_sbuses;eidx++)
-		if ( huart == mySerialProtocols[eidx]->Uart)
+	for (eidx=1;eidx<=no_mySerialProtocols;eidx++)
+		if ( huart == mySerialProtocols[eidx]->func.Uart)
 		  {
+			switch (protocol_st)
+			{
+				case ST_INIT:
 
-			if (sbuses[eidx]->func.sbusTimer.cur_value > 15)
-			{
-				sbuses[eidx]->func.no_char =0;
+					protocol_st = ST_WF_STARTCHAR;
+				break;
+
+				case ST_RESET:
+					protocol_st = ST_WF_STARTCHAR;
+					break;
+
+				case ST_WF_STARTCHAR:
+					if (mySerialProtocols[eidx]->func.oneCharBuffer == mySerialProtocols[eidx]->protocol.startChar)
+					{
+						mySerialProtocols[eidx]->func.no_char++;
+						protocol_st = ST_WF_STATION;
+					}
+					break;
+
+				case ST_WF_STATION:
+						mySerialProtocols[eidx]->currentInFrame.Station =mySerialProtocols[eidx]->func.oneCharBuffer;
+						mySerialProtocols[eidx]->func.no_char++;
+						protocol_st = ST_WF_COMMAND;
+					break;
+
+				case ST_WF_COMMAND:
+					mySerialProtocols[eidx]->currentInFrame.Command =mySerialProtocols[eidx]->func.oneCharBuffer;
+					mySerialProtocols[eidx]->func.no_char++;
+					protocol_st = ST_WF_ADDRESS;
+					break;
+
+				case ST_WF_ADDRESS:
+					mySerialProtocols[eidx]->currentInFrame.Address =mySerialProtocols[eidx]->func.oneCharBuffer;
+					mySerialProtocols[eidx]->func.no_char++;
+					protocol_st =  ST_WF_VALUE;
+					break;
+
+				case ST_WF_VALUE:
+					mySerialProtocols[eidx]->currentInFrame.Value =mySerialProtocols[eidx]->func.oneCharBuffer;
+					mySerialProtocols[eidx]->func.no_char++;
+					protocol_st =  ST_WF_VALUE;
+					break;
+
+				case ST_WF_ENDCHAR:
+					if (mySerialProtocols[eidx]->func.oneCharBuffer == mySerialProtocols[eidx]->protocol.endChar)
+					mySerialProtocols[eidx]->currentInFrame.Value =mySerialProtocols[eidx]->func.oneCharBuffer;
+					mySerialProtocols[eidx]->func.no_char++;
+					protocol_st =  ST_WF_VALUE;
+					break;
 			}
-			else
-			{
-				sbuses[eidx]->func.no_char++;
-			}
+			HAL_UART_Receive_IT(huart, &(mySerialProtocols[eidx]->func.oneCharBuffer), 1);
+			/**
+			 *   Feladatok:
+			 *   	- timer ami felügyeli a két karakter közti időt
+			 *   	- ha az idő letelt ráküldi a reset folyamatra, elfelejti az eddig begyűjtött karaktereket és újra várni kezdi az elsőt
+			 */
+
+
+			/*
+
+
 			sbuses[eidx]->func.SBUS_rxBuffer[sbuses[eidx]->func.no_char]=sbuses[eidx]->func.oneCharBuffer;
 			parseSbusMessage(sbuses[eidx],sbuses[eidx]->func.SBUS_rxBuffer);
 			HAL_UART_Receive_IT(huart, &(sbuses[eidx]->func.oneCharBuffer), 1);
 			setTimer(&(sbuses[eidx]->func.sbusTimer));
+			*/
 		  }
 }
